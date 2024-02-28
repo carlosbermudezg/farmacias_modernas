5c1bd417c9ac23a483fe5467237fcf95328f84a9
Carlos Bermudez <cbermudezg7@gmail.com>
2024-02-28 11:43:36 -0500

    Variables de entorno

--- src/utils/connMySql2.js
+++ src/utils/connMySql2.js
@@ -1,12 +1,13 @@
 const mysql2 = require('mysql2/promise');
+require('dotenv').config();
 
 // Connect to server
 const pool = mysql2.createPool({
-    host     : "192.168.2.172",
-    user     : "root",
-    password : "miguel66677710101418/2=golosos",
-    port     : "3306",
-    database : "farmacia_lopez",
+    host     : process.env.HOST,
+    user     : process.env.USER,
+    password : process.env.DB_PASSWORD,
+    port     : process.env.MYSQL_PORT,
+    database : process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
